be1cd0503e2efd2aa21ba738657f5438b5a5fe2b
Danilo <danilo.bakun@gmail.com>
2019-03-13 07:18:39 -0300

    Requisicao ajax completa

---
+++ requisicao-ajax-completa/script.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('#form').bind('submit', function(e){
+        e.preventDefault();
+
+        var txt = $(this).serialize();
+        console.log(txt);
+
+        $.ajax({
+            type:'POST',
+            url:'ajax.php',
+            data:txt,
+            success:function(resultado){
+                $('.div').html("Resultado: "+resultado);
+            },
+            error:function(){
+                alert("ocorreu um erro");
+            }
+        });
+    });
+});
